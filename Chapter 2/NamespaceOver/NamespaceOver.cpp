33db4823a3415f6a5b89373756b5e7b3c29af3d7
Su-ho Park <psuhoo122065@gmail.com>
2022-07-04 12:01:19 +0900

    Updated on Jul 04 2022 12:01

---
+++ Chapter 2/NamespaceOver/NamespaceOver.cpp
@@ -0,0 +1,31 @@
+﻿// NamespaceOver.cpp
+
+#include <iostream>
+using namespace std;
+
+// 전역 함수
+void func(void) {
+	cout << "::func()" << endl;
+}
+
+namespace Space {
+	// Space 스페이스에 소속된 함수
+	void func(void) {
+		cout << "Space::func()" << endl;
+	}
+}
+
+namespace Space2 {
+	// Space2 스페이스에 소속된 함수
+	void func(void) {
+		cout << "Space2::func()" << endl;
+	}
+}
+
+int main()
+{
+	func(); // 묵시적 전역
+	::func(); // 명시적 전역
+	Space::func();
+	Space2::func();
+}
