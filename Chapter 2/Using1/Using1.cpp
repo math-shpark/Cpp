925ad85989d7fede69009a3c2f1244dc0c762668
Su-ho Park <psuhoo122065@gmail.com>
2022-07-06 00:00:44 +0900

    Updated on Jul 06 2022 00:00

---
+++ Chapter 2/Using1/Using1.cpp
@@ -0,0 +1,15 @@
+﻿// Using1.cpp
+
+#include <iostream>
+using namespace std;
+
+int newData = 100;
+
+namespace Test {
+	int newData = 200;
+}
+
+int main()
+{
+	cout << newData << endl;
+}
---
+++ Chapter 2/Using2/Using2.cpp
@@ -0,0 +1,17 @@
+﻿// Using2.cpp
+
+#include <iostream>
+using namespace std;
+
+int newData = 100;
+
+namespace Test {
+	int newData = 200;
+}
+
+using namespace Test;
+
+int main()
+{
+	cout << newData << endl;
+}
