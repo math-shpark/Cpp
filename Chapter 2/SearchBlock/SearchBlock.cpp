8e8d22292fdca1a0670c3a054b0d900281d985a4
Su-ho Park <psuhoo122065@gmail.com>
2022-07-05 09:18:39 +0900

    Updated on Jul 05 2022 09:18

---
+++ Chapter 2/SearchBlock/SearchBlock.cpp
@@ -0,0 +1,14 @@
+﻿// SearchBlock.cpp
+
+#include <iostream>
+using namespace std;
+
+int data(50);
+
+int main(int argc)
+{
+	int data(25);
+
+	cout << data << endl;
+	cout << argc << endl;
+}
---
+++ Chapter 2/SearchBlock2/SearchBlock2.cpp
@@ -0,0 +1,20 @@
+﻿// SearchBlock2.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int input = 0;
+
+	cout << "10 이상의 정수를 입력하세요." << endl;
+
+	cin >> input;
+
+	if (input >= 10) {
+		cout << input << endl;
+	}
+	else {
+		cout << "잘못 입력하셨습니다." << endl;
+	}
+}
