d42d2d4271f7b9ba28a1cb13958cc16408b72bd1
Su-ho Park <psuhoo122065@gmail.com>
2022-07-13 22:39:31 +0900

    feat : example code about constructor and destructor

---
+++ Chapter 3/Constructor1/Constructor1.cpp
@@ -0,0 +1,22 @@
+﻿// Constructor1.cpp
+
+#include <iostream>
+using namespace std;
+
+class Test {
+public:
+	Test() {
+		cout << "Start" << endl;
+	}
+
+	~Test() {
+		cout << "End" << endl;
+	}
+};
+
+int main()
+{
+	cout << "main() start" << endl;
+	Test test;
+	cout << "main() end" << endl;
+}
