c793c71000f123a255ba6a6e758c8300777b52b7
Su-ho Park <psuhoo122065@gmail.com>
2022-07-18 00:03:07 +0900

    FEAT : Sample Code of Default Constructor

---
+++ Chapter 3/DefaultConstructor/DefaultConstructor.cpp
@@ -0,0 +1,17 @@
+﻿// DefaultConstructor.cpp
+
+#include <iostream>
+using namespace std;
+
+class Test {
+public:
+	Test(void) = default;
+
+	int data = 10;
+};
+
+int main()
+{
+	Test test;
+	cout << test.data << endl;
+}
