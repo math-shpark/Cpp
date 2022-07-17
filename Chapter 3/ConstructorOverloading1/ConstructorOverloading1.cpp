17f5a6aba2430f993f1b4392887e512fce508d09
Su-ho Park <psuhoo122065@gmail.com>
2022-07-17 20:20:53 +0900

    FEAT : Sample Code 1 of Constructor Overloading

---
+++ Chapter 3/ConstructorOverloading1/ConstructorOverloading1.cpp
@@ -0,0 +1,31 @@
+﻿// ConstructorOverloading1.cpp
+
+#include <iostream>
+using namespace std;
+
+class MyData {
+public:
+	MyData(int param) : newData(param) {
+
+	};
+
+	MyData(int param1, int param2) : newData(param1 + param2) {
+
+	};
+
+	int getData(void) {
+		return newData;
+	}
+
+private:
+	int newData;
+};
+
+int main()
+{
+	MyData data1(10);
+	MyData data2(5, 10);
+
+	cout << data1.getData() << endl;
+	cout << data2.getData() << endl;
+}
