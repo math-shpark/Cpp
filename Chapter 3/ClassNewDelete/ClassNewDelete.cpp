cac13801437615a64dd1db090bb9858ea205efe4
Su-ho Park <psuhoo122065@gmail.com>
2022-07-17 15:33:14 +0900

    FEAT : Sample code of New and Delete of class

---
+++ Chapter 3/ClassNewDelete/ClassNewDelete.cpp
@@ -0,0 +1,29 @@
+﻿// ClassNewDelete.cpp
+
+#include <iostream>
+using namespace std;
+
+class Test {
+	int testData;
+public:
+	Test() {
+		cout << "Test::Test()" << endl;
+	}
+
+	~Test() {
+		cout << "~Test::Test()" << endl;
+	}
+};
+
+int main()
+{
+	cout << "main() begin" << endl;
+
+	// new
+	Test* data = new Test;
+	cout << "Test" << endl;
+
+	// delete
+	delete data;
+	cout << "end" << endl;
+}
